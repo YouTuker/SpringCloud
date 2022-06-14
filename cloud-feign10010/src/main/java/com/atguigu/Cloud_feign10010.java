9f9bfa83e7afba14ebfd29dda36e3c28f7bf9daf
yangchuantang <layne@atguigu.com>
2022-06-15 02:07:09 +0800

    调整项目

--- cloud-feign10010/src/main/java/com/atguigu/Cloud_feign10010.java
+++ cloud-feign10010/src/main/java/com/atguigu/Cloud_feign10010.java
@@ -2,12 +2,12 @@ package com.atguigu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class Cloud_feign10010 {
     public static void main(String[] args) {
                 SpringApplication.run(Cloud_feign10010.class,args);
--- cloud-feign10010/src/main/java/com/atguigu/Feign/FeignTo9001.java
+++ cloud-feign10010/src/main/java/com/atguigu/Feign/FeignTo9001.java
@@ -1,10 +1,9 @@
 package com.atguigu.Feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "cloudAlibaba-provide-payment")
+@FeignClient(name = "cloud-cosumer-6003")
 public interface FeignTo9001 {
 
     @GetMapping("/hello")
